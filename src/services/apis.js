3679a044dd93135353028725861d08a8937d78ab
ShreePrakashReck <shreeprakash2580@gmail.com>
2023-08-15 19:23:25 +0530

    ll

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = process.env.REACT_APP_BASE_URL
-const BASE_URL = "https://node-js-app-kappa.vercel.app/api/v1";
+const BASE_URL = "https://node-js-app-kappa.vercel.app";
 
 // AUTH ENDPOINTS
 export const endpoints = {
